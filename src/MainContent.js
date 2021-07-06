ae485b1360fdf5e52fbb84b8b32bb823d709d81e
Tashu <t.r.soltamuradov@mail.ru>
2021-07-06 19:00:28 +0300

    applaed prettier

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function MainContent(props) {
-  let data = props.data
-  const product = data.map( item =>
+  let data = props.data;
+  const product = data.map((item) => (
     <div className="product_block">
       <div className="product_img">
-        <img src={item.img}/>
+        <img src={item.img} />
       </div>
       <div className="price">{item.price}</div>
       <div className="title">{item.title}</div>
       <button className="button">Купить</button>
     </div>
-      )
+  ));
   return (
     <div className="content_container">
       <div className="nav_menu">
@@ -19,9 +19,7 @@ function MainContent(props) {
         <div className="nav_item">Бытовая техника</div>
         <div className="nav_item">Прочее</div>
       </div>
-      <div className="content_product">
-        {product}
-      </div>
+      <div className="content_product">{product}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './style.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./style.css";
 
-
-ReactDOM.render(
-  <App />, document.querySelector('.container')
-)
+ReactDOM.render(<App />, document.querySelector(".container"));
